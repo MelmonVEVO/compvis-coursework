b21a8cbf6be6edc4adc1ffa916b322a3b3252153
cerigriffiths6 <57669092+cerigriffiths6@users.noreply.github.com>
2023-04-21 17:58:11 +0100

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -2,9 +2,9 @@ import os
 import numpy as np
 import matplotlib, cv2
 import matplotlib.pyplot as plt
+from timeit import default_timer
+from csv import writer, QUOTE_MINIMAL
 
-# image_file = "Task3AdditionalTestDataset/images/test_image_1.png"
-# template_file="Task2Dataset/Training/png/015-barn.png"
 images_directory = "Task3AdditionalTestDataset/images"
 templates_directory = "Task2Dataset/Training/png"
 # change above as necessary
@@ -27,28 +27,28 @@ for im_filename in os.listdir(images_directory):
         searchable_images.append(readImage)
 
 # Compare each template against each test image
+timings = []
 for image in searchable_images:
+    st = default_timer()
     for template in template_images:
 
         img1 = template
         img2 = image
 
-        # img1 = cv2.imread(template_file)
-        # img2 = cv2.imread(image_file)
-
         sift = cv2.SIFT_create()
 
+        # create key points and descriptions
         kp1, des1 = sift.detectAndCompute(img1, None)
         kp2, des2 = sift.detectAndCompute(img2, None)
 
-        bf = cv2.BFMatcher(cv2.NORM_, crossCheck=True)
+        # use brute force matcher
+        bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
 
         matches = bf.match(des1, des2) # get matches
         matches = sorted(matches, key = lambda x:x.distance) # sort matches based on distance
-        threshold = 200
-        good=[]
+        threshold = 250 # maximum distance threshold, change as appropriate
+        good = []
         for i in range(len(matches)):
-            print(matches[i].distance)
             if matches[i].distance < threshold: # filter out all bad matches
                 good.append(matches[i])
 
@@ -56,7 +56,7 @@ for image in searchable_images:
             good = good[:10] # take 10 best matches
             src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
             dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0) # builder matches mask
             matchesMask = mask.ravel().tolist()
             h, w = img1.shape[:2]
             pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
@@ -72,7 +72,14 @@ for image in searchable_images:
             img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
 
             # Draw bounding box in Red
-            img3 = cv2.polylines(img3, [np.int32(dst)], True, (0, 0, 0), 3, cv2.LINE_AA)
+            img3 = cv2.polylines(img3, [np.int32(dst)], True, (0, 0, 255), 3, cv2.LINE_AA)
 
-            cv2.imshow("result", img3)
-            cv2.waitKey()
+            # cv2.imshow("result", img3)
+            # cv2.waitKey()
+
+    et = default_timer()
+    timings.append(et - st)
+
+with open('Task3_times.csv', 'w') as csvfile:
+    wt = writer(csvfile, quotechar='|', quoting=QUOTE_MINIMAL)
+    wt.writerow(timings)
